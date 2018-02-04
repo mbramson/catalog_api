bbad6490cd75fa5b0b22017df2f0f4cc5afdac36
Mathew Bramson <mathewbramson@gmail.com>
2018-02-04 18:35:02 -0500

    Add test for current_iso_8601_datetime

--- test/catalog_api/checksum_test.exs
+++ test/catalog_api/checksum_test.exs
@@ -4,10 +4,12 @@ defmodule CatalogApi.ChecksumTest do
   alias CatalogApi.Checksum
 
   describe "generate_checksum" do
-    method_name = "cart_view"
-    uuid = "b93cee9d-dd04-4154-9b5a-8768971e72b8"
-    datetime = "2013-01-01T01:30:00Z"
+    test 'generates the correct checksum' do
+      method_name = "cart_view"
+      uuid = "b93cee9d-dd04-4154-9b5a-8768971e72b8"
+      datetime = "2013-01-01T01:30:00Z"
 
-    assert Checksum.generate_checksum(method_name, uuid, datetime) == "VdMhe0wbSyIYeymMm2YvuCmK0vE="
+      assert Checksum.generate_checksum(method_name, uuid, datetime) == "VdMhe0wbSyIYeymMm2YvuCmK0vE="
+    end
   end
 end
--- test/catalog_api_test.exs
+++ test/catalog_api_test.exs
@@ -2,4 +2,18 @@ defmodule CatalogApiTest do
   use ExUnit.Case
   doctest CatalogApi
 
+  describe "current_iso_8601_datetime" do
+    test "returns an ISO-8601 formatted datetime" do
+      datetime = CatalogApi.current_iso_8601_datetime
+      IO.inspect "doing something"
+      assert is_binary(datetime)
+      assert <<_year::bytes-size(4)>>   <> "-" <>
+             <<_month::bytes-size(2)>>  <> "-" <>
+             <<_day::bytes-size(2)>>    <> "T" <>
+             <<_hour::bytes-size(2)>>   <> ":" <>
+             <<_minute::bytes-size(2)>> <> ":" <>
+             <<_second::bytes-size(2)>> <> "." <>
+             <<_rest::bytes-size(6)>>   <> "Z" = datetime
+    end
+  end
 end
