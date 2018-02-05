ba772780ee489759594d5ab7ae267e97e99527f1
Mathew Bramson <mathewbramson@gmail.com>
2018-02-04 20:27:06 -0500

    Add test for credential_params/1

--- test/catalog_api/url_test.exs
+++ test/catalog_api/url_test.exs
@@ -4,6 +4,13 @@ defmodule CatalogApi.UrlTest do
 
   alias CatalogApi.Url
 
+  describe "credential_params/1" do
+    test "builds properly formatted params" do
+      params = Url.credential_params("view_cart")
+      assert [datetime, uuid, checksum] = params |> String.split("&")
+    end
+  end
+
   describe "base_url/0" do
     test "compiles using testing application environment" do
       assert "https://test-user.dev.catalogapi.com/v1/rest/" == Url.base_url()
