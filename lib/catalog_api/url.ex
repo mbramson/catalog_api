4c27f6df80052d40521441109e2a74cad9f34887
Mathew Bramson <mathewbramson@gmail.com>
2018-02-04 22:14:47 -0500

    Add CatalogApi.Url.url_for/2

--- lib/catalog_api/url.ex
+++ lib/catalog_api/url.ex
@@ -1,15 +1,48 @@
 defmodule CatalogApi.Url do
   alias CatalogApi.Credentials
 
+  def url_for(method, extra_params \\ %{}) do
+    cred_params = credential_params(method)
+    encoded_params = cred_params <> URI.encode_query(extra_params)
+    base_url(method) <> "?" <> encoded_params
+  end
+
+  def base_url(method) do
+    {username, environment} = retrieve_username_and_environment()
+   "https://#{username}.#{environment}.catalogapi.com/v1/rest/#{method}"
+  end
+
+  defp retrieve_username_and_environment() do
+    username = Application.get_env(:catalog_api, :username)
+    if is_nil(username) do
+      raise """
+      no :username key set in application configuration.
+      Please set the :username key in config for :catalog_api in config/config.exs
+      """
+    end
+    environment = case Application.get_env(:catalog_api, :environment) do
+      nil ->
+        raise """
+        no :environment key set in application configuration.
+        Please set the :environment key in config for :catalog_api in config/config.exs
+        """
+      "dev" -> "dev"
+      "prod" -> "prod"
+      _ ->
+        raise """
+        invalid :environment key set in application configuration.
+        value must be "dev" or "prod".
+        """
+    end
+    {username, environment}
+  end
+
+  defp validate_username_and_environment(username, environment) do
+  end
+
   def credential_params(method) do
     method
     |> Credentials.creds_for_request
     |> URI.encode_query
-  end
-
-  def base_url do
-    username = Application.get_env(:catalog_api, :username)
-    environment = Application.get_env(:catalog_api, :environment)
-   "https://#{username}.#{environment}.catalogapi.com/v1/rest/"
   end
 end
--- test/catalog_api/url_test.exs
+++ test/catalog_api/url_test.exs
@@ -5,6 +5,12 @@ defmodule CatalogApi.UrlTest do
   alias CatalogApi.Url
   import CatalogApi.FormatHelper
 
+  describe "url_for/2" do
+    test "produces correct url with no extra parameters" do
+      url = Url.url_for("view_cart")
+    end
+  end
+
   describe "credential_params/1" do
     test "builds properly formatted params" do
       params = Url.credential_params("view_cart")
@@ -20,7 +26,7 @@ defmodule CatalogApi.UrlTest do
 
   describe "base_url/0" do
     test "compiles using testing application environment" do
-      assert "https://test-user.dev.catalogapi.com/v1/rest/" == Url.base_url()
+      assert "https://test-user.dev.catalogapi.com/v1/rest/view_cart" == Url.base_url("view_cart")
     end
   end
 end
