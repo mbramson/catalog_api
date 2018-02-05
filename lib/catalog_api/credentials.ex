195c9931195f230ff118f2f3ad22ec22322899c8
Mathew Bramson <mathewbramson@gmail.com>
2018-02-04 19:41:31 -0500

    Move credential related functions to Credentials

--- lib/catalog_api/credentials.ex
+++ lib/catalog_api/credentials.ex
@@ -1,5 +1,26 @@
 defmodule CatalogApi.Credentials do
 
+  @doc """
+  Produces a tuple containing the uuid, datetime, and checksum necessary to
+  provide credentials to make and authenticated request to CatalogApi.
+  """
+  @spec creds_for_request(String.t) :: {String.t, String.t, Strin.t}
+  def creds_for_request(method) do
+    creds_datetime = current_iso_8601_datetime()
+    creds_uuid     = generate_uuid()
+    creds_checksum = Checksum.generate_checksum(method, creds_uuid, creds_datetime)
+    {creds_datetime, creds_uuid, creds_checksum}
+  end
+
+  def current_iso_8601_datetime do
+    DateTime.utc_now
+    |> DateTime.to_iso8601
+  end
+
+  def generate_uuid do
+    UUID.uuid4()
+  end
+
   def generate_checksum(method_name, uuid, iso_8601_datetime) do
     message = method_name <> uuid <> iso_8601_datetime
     key = Application.get_env(:catalog_api, :secret_key)
--- test/catalog_api/credentials_test.exs
+++ test/catalog_api/credentials_test.exs
@@ -3,6 +3,20 @@ defmodule CatalogApi.CredentialsTest do
   doctest CatalogApi.Credentials
   alias CatalogApi.Credentials
 
+  describe "current_iso_8601_datetime/0" do
+    test "returns an ISO-8601 formatted datetime" do
+      datetime = CatalogApi.current_iso_8601_datetime
+      assert is_binary(datetime)
+      assert <<_year::bytes-size(4)>>   <> "-" <>
+             <<_month::bytes-size(2)>>  <> "-" <>
+             <<_day::bytes-size(2)>>    <> "T" <>
+             <<_hour::bytes-size(2)>>   <> ":" <>
+             <<_minute::bytes-size(2)>> <> ":" <>
+             <<_second::bytes-size(2)>> <> "." <>
+             <<_rest::bytes-size(6)>>   <> "Z" = datetime
+    end
+  end
+
   describe "generate_checksum" do
     test 'generates the correct checksum' do
       method_name = "cart_view"
