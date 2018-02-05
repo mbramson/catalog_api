6ee6ebddd394989f8e7ca54f1fbcad80b8bfc426
Mathew Bramson <mathewbramson@gmail.com>
2018-02-04 19:38:51 -0500

    Add creds_for_request/1

--- lib/catalog_api.ex
+++ lib/catalog_api.ex
@@ -1,5 +1,19 @@
 defmodule CatalogApi do
 
+  alias CatalogApi.Checksum
+
+  @doc """
+  Produces a tuple containing the uuid, datetime, and checksum necessary to
+  provide credentials to make and authenticated request to CatalogApi.
+  """
+  @spec creds_for_request(String.t) :: {String.t, String.t, Strin.t}
+  def creds_for_request(method) do
+    creds_datetime = current_iso_8601_datetime()
+    creds_uuid     = generate_uuid()
+    creds_checksum = Checksum.generate_checksum(method, creds_uuid, creds_datetime)
+    {creds_datetime, creds_uuid, creds_checksum}
+  end
+
   def current_iso_8601_datetime do
     DateTime.utc_now
     |> DateTime.to_iso8601
