0af8e45120578b513c73fe7216b641b2896fedfd
Mathew Bramson <mathewbramson@gmail.com>
2018-02-11 22:32:53 -0500

    Add more item tests

--- test/catalog_api/item_test.exs
+++ test/catalog_api/item_test.exs
@@ -27,5 +27,29 @@ defmodule CatalogApi.ItemTest do
     test "produces an Item struct from json" do
       assert %Item{} = Item.cast(@base_item_json)
     end
+
+    test "coerces the has_options parameter to boolean" do
+      json = Map.put(@base_item_json, "has_options", 0)
+      assert %Item{has_options: false} = Item.cast(json)
+      json = Map.put(@base_item_json, "has_options", "0")
+      assert %Item{has_options: false} = Item.cast(json)
+      json = Map.put(@base_item_json, "has_options", 1)
+      assert %Item{has_options: true} = Item.cast(json)
+      json = Map.put(@base_item_json, "has_options", "1")
+      assert %Item{has_options: true} = Item.cast(json)
+    end
+
+    test "coerces to an error value if not coercable to boolean" do
+      json = Map.put(@base_item_json, "has_options", 123)
+      assert %Item{has_options: {:error, :failed_boolean_coercion}}
+        = Item.cast(json)
+    end
+
+    test "does not add invalid keys" do
+      json = Map.put(@base_item_json, "bad_param", 123)
+      result = Item.cast(json)
+      refute Map.get(result, :bad_param)
+      refute Map.get(result, "bad_param")
+    end
   end
 end
