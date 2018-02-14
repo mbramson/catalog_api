99dd088af60fc89b54e53291c97f151dbd5818d8
Mathew Bramson <mathewbramson@gmail.com>
2018-02-13 22:01:45 -0500

    Change version to 0.0.1

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ by adding `catalog_api` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:catalog_api, "~> 0.1.0"}
+    {:catalog_api, "~> 0.0.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CatalogApi.Mixfile do
   def project do
     [
       app: :catalog_api,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
