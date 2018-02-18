490cc1e094f4ff157194fe46e39ada83fe86db99
Mathew Bramson <mathewbramson@gmail.com>
2018-02-18 17:48:19 -0500

    Add street address validation

--- lib/catalog_api/address.ex
+++ lib/catalog_api/address.ex
@@ -32,22 +32,34 @@ defmodule CatalogApi.Address do
 
   def validate_field(:first_name, ""), do: [{:first_name, ["cannot be blank"]}]
   def validate_field(:first_name, first_name) when is_binary(first_name) do
-    cond do
-      String.length(first_name) > 40 ->
-        [{:first_name, ["cannot be longer than 40 characters"]}]
-      true -> []
-    end
+    validate_field_length(:first_name, first_name, 40)
   end
   def validate_field(:first_name, _), do: [{:first_name, ["must be a string"]}]
   def validate_field(:last_name, ""), do: [{:last_name, ["cannot be blank"]}]
   def validate_field(:last_name, last_name) when is_binary(last_name) do
+    validate_field_length(:last_name, last_name, 40)
+  end
+  def validate_field(:last_name, _), do: [{:last_name, ["must be a string"]}]
+  def validate_field(:address_1, ""), do: [{:address_1, ["cannot be blank"]}]
+  def validate_field(:address_1, address_1) when is_binary(address_1) do
+    validate_field_length(:address_1, address_1, 75)
+  end
+  def validate_field(:address_1, _), do: [{:address_1, ["must be a string"]}]
+  def validate_field(:address_2, address_2) when is_binary(address_2) do
+    validate_field_length(:address_2, address_2, 60)
+  end
+  def validate_field(:address_2, _), do: [{:address_2, ["must be a string"]}]
+  def validate_field(:address_3, address_3) when is_binary(address_3) do
+    validate_field_length(:address_3, address_3, 60)
+  end
+  def validate_field(:address_3, _), do: [{:address_3, ["must be a string"]}]
+  def validate_field(_field, _value), do: []
+
+  defp validate_field_length(field, value, max_length) when is_binary(value) do
     cond do
-      String.length(last_name) > 40 ->
-        [{:last_name, ["cannot be longer than 40 characters"]}]
+      String.length(value) > max_length ->
+        [{field, ["cannot be longer than #{max_length} characters"]}]
       true -> []
     end
   end
-  def validate_field(:last_name, _), do: [{:last_name, ["must be a string"]}]
-  def validate_field(_field, _value), do: []
-
 end
--- test/catalog_api/address_test.exs
+++ test/catalog_api/address_test.exs
@@ -26,7 +26,7 @@ defmodule CatalogApi.AddressTest do
     end
 
     test "returns an error tuple if first_name is longer than 40 characters" do
-      first_name = "12345678901234567890123456789012345678901"
+      first_name = "<  ten   ><  ten   ><  ten   ><  ten   >1"
       address = Map.put(@valid_address, :first_name, first_name)
       error_message = "cannot be longer than 40 characters"
       assert {:error, {:invalid_address, [{:first_name, [^error_message]}]}} =
@@ -50,7 +50,7 @@ defmodule CatalogApi.AddressTest do
     end
 
     test "returns an error tuple if last_name is longer than 40 characters" do
-      last_name = "12345678901234567890123456789012345678901"
+      last_name = "<  ten   ><  ten   ><  ten   ><  ten   >1"
       address = Map.put(@valid_address, :last_name, last_name)
       error_message = "cannot be longer than 40 characters"
       assert {:error, {:invalid_address, [{:last_name, [^error_message]}]}} =
@@ -61,6 +61,74 @@ defmodule CatalogApi.AddressTest do
       address = Map.put(@valid_address, :last_name, 123)
       error_message = "must be a string"
       assert {:error, {:invalid_address, [{:last_name, [^error_message]}]}} =
+        Address.validate(address)
+    end
+
+    # address_1 validation
+
+    test "returns an error tuple if address_1 is blank" do
+      address = Map.put(@valid_address, :address_1, "")
+      error_message = "cannot be blank"
+      assert {:error, {:invalid_address, [{:address_1, [^error_message]}]}} =
+        Address.validate(address)
+    end
+
+    test "returns an error tuple if address_1 is longer than 75 characters" do
+      address_1 = "<  ten   ><  ten   ><  ten   ><  ten   ><  ten   ><  ten   ><  ten   >123456"
+      address = Map.put(@valid_address, :address_1, address_1)
+      error_message = "cannot be longer than 75 characters"
+      assert {:error, {:invalid_address, [{:address_1, [^error_message]}]}} =
+        Address.validate(address)
+    end
+
+    test "returns an error tuple if address_1 is not a string" do
+      address = Map.put(@valid_address, :address_1, 123)
+      error_message = "must be a string"
+      assert {:error, {:invalid_address, [{:address_1, [^error_message]}]}} =
+        Address.validate(address)
+    end
+
+    # address_2 validation
+
+    test "returns :ok if address_2 is blank" do
+      address = Map.put(@valid_address, :address_2, "")
+      assert :ok = Address.validate(address)
+    end
+
+    test "returns an error tuple if address_2 is longer than 60 characters" do
+      address_2 = "<  ten   ><  ten   ><  ten   ><  ten   ><  ten   ><  ten   >1"
+      address = Map.put(@valid_address, :address_2, address_2)
+      error_message = "cannot be longer than 60 characters"
+      assert {:error, {:invalid_address, [{:address_2, [^error_message]}]}} =
+        Address.validate(address)
+    end
+
+    test "returns an error tuple if address_2 is not a string" do
+      address = Map.put(@valid_address, :address_2, 123)
+      error_message = "must be a string"
+      assert {:error, {:invalid_address, [{:address_2, [^error_message]}]}} =
+        Address.validate(address)
+    end
+
+    # address_3 validation
+
+    test "returns :ok if address_3 is blank" do
+      address = Map.put(@valid_address, :address_3, "")
+      assert :ok = Address.validate(address)
+    end
+
+    test "returns an error tuple if address_3 is longer than 60 characters" do
+      address_3 = "<  ten   ><  ten   ><  ten   ><  ten   ><  ten   ><  ten   >1"
+      address = Map.put(@valid_address, :address_3, address_3)
+      error_message = "cannot be longer than 60 characters"
+      assert {:error, {:invalid_address, [{:address_3, [^error_message]}]}} =
+        Address.validate(address)
+    end
+
+    test "returns an error tuple if address_3 is not a string" do
+      address = Map.put(@valid_address, :address_3, 123)
+      error_message = "must be a string"
+      assert {:error, {:invalid_address, [{:address_3, [^error_message]}]}} =
         Address.validate(address)
     end
   end
