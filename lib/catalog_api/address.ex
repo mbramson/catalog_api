fdc8badc23ec5baa43fe3cc244c455c5e5b4ba97
Mathew Bramson <mathewbramson@gmail.com>
2018-03-04 12:51:16 -0500

    Improve address validation failure error structure
    Change the format of :invalid_address error tuples returned by address
    validation from
    
    {:error, {:invalid_address, [{:field, ["something wrong"]}]}}
    
    to
    
    {:error, {:invalid_address, %{field: ["something wrong"]}}}
    
    This better matches the error returns from Ecto.Changesets and makes
    this a bit more user friendly to properly format in consuming
    applications so that field validations match those returned by Ecto
    Changeset errors rendered to the user.

--- lib/catalog_api/address.ex
+++ lib/catalog_api/address.ex
@@ -153,7 +153,7 @@ defmodule CatalogApi.Address do
       ...>   postal_code: "44444",
       ...>   country: "AJ"}
       ...> CatalogApi.Address.validate(address)
-      {:error, {:invalid_address, [{:country, ["country code must be valid ISO 3166-1 alpha 2 country code"]}, {:city, ["cannot be blank"]}]}}
+      {:error, {:invalid_address, %{country: ["country code must be valid ISO 3166-1 alpha 2 country code"], city: ["cannot be blank"]}}}
 
   """
   @spec validate(t) ::
@@ -162,12 +162,13 @@ defmodule CatalogApi.Address do
   def validate(%Address{} = address) do
     {:ok, allowed_fields} = StructHelper.allowed_fields(Address)
     errors = allowed_fields
-      |> Enum.reduce([], fn field, acc ->
-           validate_field(field, Map.fetch!(address, field)) ++ acc
+      |> Enum.reduce(%{}, fn field, acc ->
+           maybe_errors = validate_field(field, Map.fetch!(address, field))
+           Map.merge(acc, maybe_errors)
          end)
 
     case errors do
-      [] -> :ok
+      map when map == %{} -> :ok
       errors -> {:error, {:invalid_address, errors}}
     end
   end
@@ -182,74 +183,74 @@ defmodule CatalogApi.Address do
   Validates a specific address field in the context of what is valid as input
   to a CatalogApi address.
   """
-  @spec validate_field(atom(), any()) :: list()
-  def validate_field(:first_name, ""), do: [{:first_name, ["cannot be blank"]}]
+  @spec validate_field(atom(), any()) :: map()
+  def validate_field(:first_name, ""), do: %{first_name: ["cannot be blank"]}
   def validate_field(:first_name, first_name) when is_binary(first_name) do
     validate_field_length(:first_name, first_name, 40)
   end
-  def validate_field(:first_name, _), do: [{:first_name, ["must be a string"]}]
-  def validate_field(:last_name, ""), do: [{:last_name, ["cannot be blank"]}]
+  def validate_field(:first_name, _), do: %{first_name: ["must be a string"]}
+  def validate_field(:last_name, ""), do: %{last_name: ["cannot be blank"]}
   def validate_field(:last_name, last_name) when is_binary(last_name) do
     validate_field_length(:last_name, last_name, 40)
   end
-  def validate_field(:last_name, _), do: [{:last_name, ["must be a string"]}]
-  def validate_field(:address_1, ""), do: [{:address_1, ["cannot be blank"]}]
+  def validate_field(:last_name, _), do: %{last_name: ["must be a string"]}
+  def validate_field(:address_1, ""), do: %{address_1: ["cannot be blank"]}
   def validate_field(:address_1, address_1) when is_binary(address_1) do
     validate_field_length(:address_1, address_1, 75)
   end
-  def validate_field(:address_1, _), do: [{:address_1, ["must be a string"]}]
+  def validate_field(:address_1, _), do: %{address_1: ["must be a string"]}
   def validate_field(:address_2, address_2) when is_binary(address_2) do
     validate_field_length(:address_2, address_2, 60)
   end
-  def validate_field(:address_2, _), do: [{:address_2, ["must be a string"]}]
+  def validate_field(:address_2, _), do: %{address_2: ["must be a string"]}
   def validate_field(:address_3, address_3) when is_binary(address_3) do
     validate_field_length(:address_3, address_3, 60)
   end
-  def validate_field(:address_3, _), do: [{:address_3, ["must be a string"]}]
-  def validate_field(:city, ""), do: [{:city, ["cannot be blank"]}]
+  def validate_field(:address_3, _), do: %{address_3: ["must be a string"]}
+  def validate_field(:city, ""), do: %{city: ["cannot be blank"]}
   def validate_field(:city, city) when is_binary(city) do
     validate_field_length(:city, city, 40)
   end
-  def validate_field(:city, _), do: [{:city, ["must be a string"]}]
-  def validate_field(:state_province, ""), do: [{:state_province, ["cannot be blank"]}]
+  def validate_field(:city, _), do: %{city: ["must be a string"]}
+  def validate_field(:state_province, ""), do: %{state_province: ["cannot be blank"]}
   def validate_field(:state_province, state_province) when is_binary(state_province) do
     validate_field_length(:state_province, state_province, 50)
   end
-  def validate_field(:state_province, _), do: [{:state_province, ["must be a string"]}]
-  def validate_field(:postal_code, ""), do: [{:postal_code, ["cannot be blank"]}]
+  def validate_field(:state_province, _), do: %{state_province: ["must be a string"]}
+  def validate_field(:postal_code, ""), do: %{postal_code: ["cannot be blank"]}
   def validate_field(:postal_code, postal_code) when is_binary(postal_code) do
     validate_field_length(:postal_code, postal_code, 15)
   end
-  def validate_field(:postal_code, _), do: [{:postal_code, ["must be a string"]}]
-  def validate_field(:country, ""), do: [{:country, ["cannot be blank"]}]
+  def validate_field(:postal_code, _), do: %{postal_code: ["must be a string"]}
+  def validate_field(:country, ""), do: %{country: ["cannot be blank"]}
   def validate_field(:country, country) when is_binary(country) do
     case Iso3166.validate(country) do
-      :ok -> []
-      :error -> [{:country, ["country code must be valid ISO 3166-1 alpha 2 country code"]}]
+      :ok -> %{}
+      :error -> %{country: ["country code must be valid ISO 3166-1 alpha 2 country code"]}
     end
   end
-  def validate_field(:country, _), do: [{:country, ["must be a string"]}]
-  def validate_field(:email, ""), do: []
+  def validate_field(:country, _), do: %{country: ["must be a string"]}
+  def validate_field(:email, ""), do: %{}
   def validate_field(:email, email) when is_binary(email) do
     cond do
-      String.length(email) > 254 -> [{:email, ["cannot be longer than 254 characters"]}]
-      Email.valid?(email) -> []
-      true -> [{:email, ["must be a valid email"]}]
+      String.length(email) > 254 -> %{email: ["cannot be longer than 254 characters"]}
+      Email.valid?(email) -> %{}
+      true -> %{email: ["must be a valid email"]}
     end
   end
-  def validate_field(:email, _), do: [{:email, ["must be a string"]}]
-  def validate_field(:phone_number, ""), do: []
+  def validate_field(:email, _), do: %{email: ["must be a string"]}
+  def validate_field(:phone_number, ""), do: %{}
   def validate_field(:phone_number, phone_number) when is_binary(phone_number) do
     validate_field_length(:phone_number, phone_number, 20)
   end
-  def validate_field(:phone_number, _), do: [{:phone_number, ["must be a string"]}]
-  def validate_field(_field, _value), do: []
+  def validate_field(:phone_number, _), do: %{phone_number: ["must be a string"]}
+  def validate_field(_field, _value), do: %{}
 
   defp validate_field_length(field, value, max_length) when is_binary(value) do
     cond do
       String.length(value) > max_length ->
-        [{field, ["cannot be longer than #{max_length} characters"]}]
-      true -> []
+        %{field => ["cannot be longer than #{max_length} characters"]}
+      true -> %{}
     end
   end
 end
