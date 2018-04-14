ca9566f82ce542b1396b2ea8157d7d9823c504f9
Mathew Bramson <mathewbramson@gmail.com>
2018-04-14 11:29:28 -0400

    Make CatalogApi.cart_set_address accepts struct
    CatalogApi.cart_set_address documentation specified that it accepted a
    CatalogApi.Address{} struct, but it in fact errored. Ensure that it
    accepts this struct without erroring.
    
    Add a CatalogApi.Address.fake_valid_address method which returns a valid
    address struct. This make testing address related functionality a bit
    easier.

--- lib/catalog_api.ex
+++ lib/catalog_api.ex
@@ -152,6 +152,10 @@ defmodule CatalogApi do
     | {:error, {:catalog_api_fault, Error.extracted_fault}}
     | {:error, Poison.ParseError.t}
     | {:error, :unparseable_response_description}
+  def cart_set_address(socket_id, external_user_id, address = %Address{}) do
+    address_params = Map.from_struct(address)
+    cart_set_address(socket_id, external_user_id, address_params)
+  end
   def cart_set_address(socket_id, external_user_id, address_params) do
     with :ok <- Address.validate_params(address_params) do
       params = address_params
@@ -267,12 +271,14 @@ defmodule CatalogApi do
     order has not changed since the application's state has been updated.
   """
   @spec cart_view(integer(), integer()) ::
-    {:ok, %{items: list(Item.t), status: map() | :cart_status_unavailable}} | {:error, atom()}
+    {:ok, %{items: list(Item.t), status: map() | :cart_status_unavailable}}
+    | {:error, atom()}
   def cart_view(socket_id, external_user_id) do
     params = %{socket_id: socket_id, external_user_id: external_user_id}
     url = Url.url_for("cart_view", params)
     with {:ok, response} <- HTTPoison.get(url),
          :ok <- Error.validate_response_status(response),
+         _ <- IO.inspect(response),
          {:ok, json} <- parse_json(response.body),
          {:ok, items} <- CartItem.extract_items_from_json(json),
          {:ok, cart_status} <- extract_cart_status(json) do
