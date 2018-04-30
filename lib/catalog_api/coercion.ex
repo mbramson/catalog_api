fc2cb175bdd787f0aebbedf3f2f4d8c3be8bd7b9
Mathew Bramson <mathewbramson@gmail.com>
2018-04-29 22:30:18 -0400

    Add coercion to integer from boolean

--- lib/catalog_api/coercion.ex
+++ lib/catalog_api/coercion.ex
@@ -1,8 +1,6 @@
 defmodule CatalogApi.Coercion do
   @moduledoc false
 
-  #TODO: Add Tests
-
   @spec integer_fields_to_boolean(map(), list(String.t | atom()), boolean()) :: map()
   def integer_fields_to_boolean(map, boolean_fields, return_map \\ false) do
     fields = Enum.map(map, fn {k, v} -> coerce_to_boolean_if_needed(k, v, boolean_fields) end)
@@ -27,4 +25,30 @@ defmodule CatalogApi.Coercion do
   defp coerce_integer_to_boolean("0"), do: false
   defp coerce_integer_to_boolean("1"), do: true
   defp coerce_integer_to_boolean(_), do: {:error, :failed_boolean_coercion}
+
+  @spec boolean_fields_to_integer(map(), list(String.t | atom()), boolean()) :: map()
+  def boolean_fields_to_integer(map, boolean_fields, return_map \\ false) do
+    fields = Enum.map(map, fn {k,v} -> coerce_to_integer_if_needed(k, v, boolean_fields) end)
+    if return_map do
+      fields |> Enum.into(%{})
+    else
+      fields
+    end
+  end
+
+  @spec coerce_to_integer_if_needed(String.t | atom(), any(), list(Strin.t | atom())) ::
+    {String.t, any()}
+  defp coerce_to_integer_if_needed(key, value, boolean_fields) do
+    cond do
+      key in boolean_fields -> {key, coerce_boolean_to_integer(value)}
+      true -> {key, value}
+    end
+  end
+
+  @spec coerce_boolean_to_integer(any()) :: integer() | {:error, :failed_integer_coercion}
+  defp coerce_boolean_to_integer(true), do: 1
+  defp coerce_boolean_to_integer("true"), do: 1
+  defp coerce_boolean_to_integer(false), do: 0
+  defp coerce_boolean_to_integer("false"), do: 0
+  defp coerce_boolean_to_integer(_), do: {:error, :failed_integer_coercion}
 end
--- test/catalog_api/coercion_test.exs
+++ test/catalog_api/coercion_test.exs
@@ -29,4 +29,31 @@ defmodule CatalogApi.CoercionTest do
         Coercion.integer_fields_to_boolean(map, boolean_fields)
     end
   end
+
+  describe "boolean_fields_to_integer/3" do
+    test "coerces a field to an integer value" do
+      boolean_fields = [:bool]
+      map = %{bool: true, num: true}
+      assert [bool: 1, num: true] = Coercion.boolean_fields_to_integer(map, boolean_fields)
+      map = %{bool: "true", num: true}
+      assert [bool: 1, num: true] = Coercion.boolean_fields_to_integer(map, boolean_fields)
+      map = %{bool: false, num: true}
+      assert [bool: 0, num: true] = Coercion.boolean_fields_to_integer(map, boolean_fields)
+      map = %{bool: "false", num: true}
+      assert [bool: 0, num: true] = Coercion.boolean_fields_to_integer(map, boolean_fields)
+    end
+
+    test "returns a map if return_map is true" do
+      boolean_fields = [:bool]
+      map = %{bool: true, num: true}
+      assert %{bool: 1, num: true} = Coercion.boolean_fields_to_integer(map, boolean_fields, true)
+    end
+
+    test "returns an error tuple for the field if it cannot be coerced" do
+      boolean_fields = [:bool]
+      map = %{bool: "maybe"}
+      assert [bool: {:error, :failed_integer_coercion}] =
+        Coercion.boolean_fields_to_integer(map, boolean_fields)
+    end
+  end
 end
