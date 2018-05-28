bfc372f4566e98490778710c404a212597ab3c4c
Mathew Bramson <mathewbramson@gmail.com>
2018-05-28 14:42:57 -0400

    Improve CatalogApi.cart_remove_item

--- lib/catalog_api.ex
+++ lib/catalog_api.ex
@@ -281,22 +281,36 @@ defmodule CatalogApi do
        }) do
     {:ok, description}
   end
+  defp extract_description(%{
+         "cart_remove_item_response" => %{
+           "cart_remove_item_result" => %{"description" => description}
+         }
+       }) do
+    {:ok, description}
+  end
 
   defp extract_description(_), do: {:error, :unparseable_response_description}
 
-  def cart_remove_item(socket_id, external_user_id, catalog_item_id, option_id, quantity) do
-    # TODO make quantity optional, as it default to current quantity
-    # TODO add option_id to optional params
-    params = %{
+  def cart_remove_item(socket_id, external_user_id, catalog_item_id, opts \\ []) do
+    allowed = [:option_id, :quantity]
+    {:ok, optional_params} = filter_optional_params([], opts, allowed)
+
+    required_params = %{
       socket_id: socket_id,
       external_user_id: external_user_id,
-      catalog_item_id: catalog_item_id,
-      option_id: option_id,
-      quantity: quantity
+      catalog_item_id: catalog_item_id
     }
 
+    params = Map.merge(optional_params, required_params)
+
     url = Url.url_for("cart_remove_item", params)
-    HTTPoison.get(url)
+
+    with {:ok, response} <- HTTPoison.get(url),
+         :ok <- Error.validate_response_status(response),
+         {:ok, json} <- parse_json(response.body),
+         {:ok, description} <- extract_description(json) do
+      {:ok, %{description: description}}
+    end
   end
 
   def cart_empty(socket_id, external_user_id) do
