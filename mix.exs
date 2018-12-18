7b40e19e30b2840bed3a6550248150dd5d76773b
Mathew Bramson <mathewbramson@gmail.com>
2018-12-17 20:55:10 -0500

    Bump version to 0.0.15

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CatalogApi.Mixfile do
   def project do
     [
       app: :catalog_api,
-      version: "0.0.14",
+      version: "0.0.15",
       elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
